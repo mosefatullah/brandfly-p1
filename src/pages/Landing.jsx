b3aec7ba74128aac3ddb2802a9807f6874b9f82d
saadalamin <saadalamin2004@gmail.com>
2024-09-24 22:38:13 +0600

    .

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -20,6 +20,7 @@ function Landing() {
   ];
   const [pricingCheck, setPricingCheck] = useState(false);
   const [pricingRadio, setPricingRadio] = useState("plan-radio-1");
+  const [formRadio, setFormRadio] = useState("form-youtube");
 
   const makeMove = (element, trigger) => {
     gsap.to(element, {
@@ -509,7 +510,7 @@ function Landing() {
           </div>
         </section>
 
-        <button className="block mx-auto bg-primary text-white text-[14px] font-medium uppercase tracking-widest py-2.5 px-4 mt-8">
+        <button className="block mx-auto bg-primary text-white text-lg font-semibold py-2.5 px-7 mt-8">
           Schedule A Free Strategy Call Now
         </button>
       </section>
@@ -849,7 +850,7 @@ function Landing() {
             ].map((p, i) => (
               <div
                 key={i}
-                className="border-[2px] border-black p-4 flex flex-col justify-between"
+                className="border-[2px] border-black p-4 flex flex-col justify-between bg-white"
               >
                 <div>
                   <h1 className="font-semibold uppercase text-3xl text-primary">
@@ -877,13 +878,108 @@ function Landing() {
                     </p>
                     <p className="font-semibold text-lg">/mo</p>
                   </div>
-                  <button className="block mx-auto bg-primary text-white text-lg font-semibold tracking-widest py-1.5 px-6 mt-8">
+                  <button className="block mx-auto bg-primary text-white text-lg font-semibold py-1.5 px-7 mt-8">
                     Select Plan
                   </button>
-                <div className="h-[80px] pt-5 font-semibold">{p.term || ""}</div>
+                  <div className="h-[80px] pt-5 font-semibold">
+                    {p.term || ""}
+                  </div>
                 </div>
               </div>
             ))}
+          </div>
+        </section>
+        <section className="bg-black bg-[url('/images/underpricing.png')] text-white bg-cover w-full pb-12 relative -z-10 rounded-t-3xl -mt-[280px] pt-[300px] px-4 text-center">
+          <h1 className="uppercase text-4xl font-bold max-w-sm mx-auto">
+            Can't find any{" "}
+            <span className="text-secondary">suitable package?</span>
+          </h1>
+
+          <p className="text-lg max-w-xl mx-auto my-5">
+            if you cant find any suitable package here you can always ask for a
+            quote by reaching us out. We are here 24/7
+          </p>
+
+          <button className="block mx-auto bg-primary text-white text-lg font-semibold py-2 px-8 mt-10">
+            Get a Quote
+          </button>
+        </section>
+
+        <section className="grid lg:grid-cols-2 gap-6 max-w-5xl mx-auto pt-12">
+          <div>
+            <h1 className="uppercase text-4xl font-bold">
+              Book a Call <span className="text-primary">Today</span>
+            </h1>
+
+            <p className="my-5 mt-6">
+              Have any query in mind? Just book a call today and we will discuss
+              them together.
+            </p>
+            <p className="my-5">
+              Save upto 70% from your first month Subscription
+            </p>
+            <p className="my-5">
+              Everyone you will hire is well trained to deliver files as you
+              want
+            </p>
+          </div>
+          <div>
+            <form action="" className="flex flex-col gap-3">
+              <div className="grid grid-cols-2 gap-3">
+                <input
+                  type="text"
+                  className="px-4 py-2.5 border-2 border-black/50 placeholder:text-black/80"
+                  placeholder="Name"
+                />
+                <input
+                  type="tel"
+                  className="px-4 py-2.5 border-2 border-black/50 placeholder:text-black/80"
+                  placeholder="Phone Number"
+                />
+              </div>
+              <input
+                type="tel"
+                className="px-4 py-2.5 border-2 border-black/50 placeholder:text-black/80"
+                placeholder="Email"
+              />
+              <h2 className="font-semibold">I Want Video Editing for</h2>
+              <div className="grid grid-cols-3 gap-3">
+                {["Youtube", "Instagram", "Other"].map((s, i) => (
+                  <label
+                    htmlFor={`form-${s.toLowerCase()}`}
+                    className={
+                      "px-4 py-2.5 border-2 border-black/50 placeholder:text-black/80 flex gap-2 items-center" +
+                      (formRadio == `form-${s.toLowerCase()}` &&
+                        " bg-primary !border-primary text-white")
+                    }
+                  >
+                    <input
+                      type="radio"
+                      id={`form-${s.toLowerCase()}`}
+                      checked={formRadio == `form-${s.toLowerCase()}`}
+                      onChange={() => setFormRadio(`form-${s.toLowerCase()}`)}
+                    />
+                    <p>{s}</p>
+                  </label>
+                ))}
+              </div>
+              <select
+                type="tel"
+                className="px-4 py-2.5 bg-white border-2 border-black/50"
+              >
+                <option value="">Select Your Budget</option>
+              </select>
+              <h2 className="font-semibold">Describe Your Project</h2>
+              <textarea
+                rows={5}
+                className="px-4 py-2.5 border-2 border-black/50 placeholder:text-black/80"
+                placeholder="Your Text"
+              ></textarea>
+
+              <button className="block mx-auto bg-primary text-white text-lg font-semibold py-1.5 px-6 mt-4">
+                Schedule A Call
+              </button>
+            </form>
           </div>
         </section>
       </section>
