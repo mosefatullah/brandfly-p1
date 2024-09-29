510447d25892ac219dc6d82b01cfb867998474f1
mosefatullah <mosefatullah@gmail.com>
2024-09-29 21:42:38 +0600

    .

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,6 +1,40 @@
 
 import Counts from './../components/Counts';
 export default function AboutUs() {
+    const services = [{
+        title: "Dividual Creators",
+        image: "serv1.png",
+        description: "Whether you're an influencer, podcaster, traveler, or vlogger, we tailor our editing services to meet your specific needs. From short-form content like TikToks and reels to long-form YouTube videos, we ensure your videos are polished and professional.",
+        tags: ["Influencer", "Podcaster", "Vlogger", "You Name It"],
+    }, {
+        title: "Project-Based Editing",
+        image: "serv2.png",
+        description: "Need editing for a specific project? We offer flexible, project-based editing services that deliver top-notch results, no matter the scope or complexity of your project.",
+        tags: ["Project-Based", "Flexible"]
+    }, {
+        title: "Monthly Subscription",
+        image: "serv3.png",
+        description: "Monthly Subscription: For creators with ongoing content needs, our monthly subscription packages provide unlimited video edits, ensuring you always have fresh, high-quality content ready to share.",
+        tags: ["Creators", "Long Time Projects", "Unlimited Video "]
+    }, {
+        title: "Single Edit Services",
+        image: "serv4.png",
+        description: "Just need one video edited? Our single edit service is perfect for creators who require occasional editing support, delivering quick turnaround times without compromising on quality.",
+        tags: ["Single Video", "Unique"]
+    }, {
+        title: "Agency Partnerships",
+        image: "serv5.png",
+        description: "We also collaborate with other agencies that need reliable video editing services for their clients. Our team seamlessly integrates with your workflow to deliver exceptional video content on time, every time.",
+        tags: ["Agency", "Flexible Time"]
+    }, {
+        title: "Anything not Mentioned?",
+        image: "serv6.png",
+        description: "Bring your questions, no matter how quirky, and let’s have fun together! Your curiosity is the spice of our day. So don’t hesitate just fire away your questions.",
+        button: {
+            text: "Let's Talk",
+            link: "#"
+        },
+    }]
     return (
         <>
             <section className='py-8'>
@@ -9,7 +43,7 @@ export default function AboutUs() {
                     <h1 className='mt-5 font-bold text-[75px] leading-[4.3rem] uppercase max-w-3xl mx-auto'>
                         your <span className='text-primary'>premier</span> video editing <span className='text-primary'>agency</span>
                     </h1>
-                    <p className='mt-5 max-w-3xl mx-auto text-lg font-gothic'>Dedicated to transforming your raw footage into compelling visual stories. We specialize in editing videos for a diverse range of clients including influencers, podcasters, travelers, vloggers, and more.</p>
+                    <p className='mt-5 max-w-3xl mx-auto text-[22px] font-gothic'>Dedicated to transforming your raw footage into compelling visual stories. We specialize in editing videos for a diverse range of clients including influencers, podcasters, travelers, vloggers, and more.</p>
                 </div>
 
             </section>
@@ -32,26 +66,83 @@ export default function AboutUs() {
                 </div>
                 <Counts />
             </section>
-            <section className='p-4'>
+            <section className='px-4 py-12'>
                 <div className='text-center'>
                     <h1 className='font-bold text-[42px] uppercase max-w-3xl mx-auto'>Explore our <span className='text-primary'>services</span></h1>
-                    <p className='mt-1.5 max-w-3xl mx-auto text-lg font-gothic'>Something about our services in a few words so that someone can get rough idea about what they are gonna experience next.</p>
+                    <p className='mt-1.5 max-w-3xl mx-auto text-[22px] font-gothic'>Something about our services in a few words so that someone can get rough idea about what they are gonna experience next.</p>
+                </div>
+
+                <div className='grid grid-cols-3 gap-4 lg:gap-8 max-w-7xl mx-auto p-8'>
+                    {services.map((i, index) => (
+                        <div key={index} className='border border-black p-4 max-w-[416px] pb-12 hover:scale-105 transition-all'>
+                            <div>
+                                <img src={`/images/${i.image}`} alt={i.title} className='w-full' />
+                            </div>
+                            <div className='mt-8'>
+                                <h1 className='text-primary font-bold text-[34px] leading-tight'>{i.title}</h1>
+                                <p className='my-4 max-w-3xl mx-auto text-[22px] font-gothic mb-8 leading-tight tracking-wide'>{i.description}</p>
+                                <div className='flex flex-wrap gap-4'>
+                                    {i?.tags?.map((tag, index) => (
+                                        <div key={index} className='hover:scale-105 active:scale-100 transition-all bg-[#EEF2F5] rounded-full text-black text-[14px] font-semibold uppercase px-4 py-1.5'>{tag}</div>
+                                    ))}
+                                </div>
+                                {i.button && (
+                                    <button className="bg-primary text-white text-[16px] font-bold tracking-widest py-2 px-12 select-none hover:scale-105 active:scale-100 font-gothic mt-5 rounded-full">
+                                        {i.button.text}
+                                    </button>
+                                )}
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </section>
-            <section className="bg-black text-white w-full px-4 py-8">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime beatae nihil vitae minima debitis quisquam deleniti necessitatibus reprehenderit eum, consectetur accusamus animi quibusdam laborum rerum inventore quas, autem, nobis mollitia?
+            <section className="bg-black text-white w-full px-4 py-16">
+                <div className="max-w-5xl mx-auto">
+                    <div className='flex flex-col lg:flex-row gap-4 lg:justify-between'>
+                        <div>
+                            <h1 className='font-bold text-[42px] uppercase max-w-xs leading-tight'>Together we are <span className='text-secondary'>strong</span></h1>
+                        </div>
+                        <div className='max-w-xl'>
+                            <p className='text-xl font-[100] tracking-wide'>
+                                <b className='font-semibold'>At HypeCut, we are passionate about storytelling and dedicated to help you bring your wildest vision into life.</b>
+                                <br /><br />
+                                With our professional editing team, cutting-edge tools, and commitment to excellence, your videos will stand out and captivate your audience.
+                                <br /><br />
+                                Ready to elevate your content? Contact us today to learn more about how HypeCut can help you create stunning videos that leave a lasting impact
+                            </p>
+                        </div>
+                    </div>
+                    <div className='flex flex-col lg:flex-row gap-4 lg:justify-between mt-14'>
+                        <div className='flex gap-2'>
+                            <img src='/images/nazif.png' alt='CEO' className='w-[70px] h-[70px] rounded-full' />
+                            <div>
+                                <h1 className='font-semibold text-[24px]'>Nazif Yealmy</h1>
+                                <p className='text-[18px] font-[100] tracking-widest font-gothic'>Founder & CEO</p>
+                            </div>
+                        </div>
+                        <div className='max-w-xl'>
+                            <p className='text-xl font-[100] tracking-wide'>
+                                <b className='font-semibold'>At HypeCut, we are passionate about storytelling and dedicated to help you bring your wildest vision into life.</b>
+                                <br /><br />
+                                With our professional editing team, cutting-edge tools, and commitment to excellence, your videos will stand out and captivate your audience.
+                                <br /><br />
+                                Ready to elevate your content? Contact us today to learn more about how HypeCut can help you create stunning videos that leave a lasting impact
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </section>
-            <section className='px-4 py-12'>
-                <div className='flex flex-col lg:flex-row gap-4 lg:gap-8 max-w-4xl mx-auto lg:items-center'>
+            <section className='px-4 py-14'>
+                <div className='flex flex-col lg:flex-row gap-4 lg:gap-8 max-w-5xl mx-auto lg:items-center'>
                     <div>
                         <h1 className='font-bold text-[42px] uppercase max-w-3xl mx-auto'><span className='text-primary'>Join</span> our team</h1>
                         <img src='/images/join-team.png' alt='Join' className='w-full mt-8' />
                     </div>
 
                     <div>
-                        <b className='max-w-3xl mx-auto text-lg font-gothic'>Are you a video editing wizard with a passion for storytelling?</b>
-                        <p className='my-5 max-w-3xl mx-auto text-lg font-gothic'>We're on the hunt for talented individuals to join our dynamic team. Hypecut is more than just a workplace; it's a creative hub where your skills can shine.</p>
-                        <button className="bg-primary text-white text-[16px] font-bold tracking-widest py-2.5 px-4 select-none hover:scale-105 active:scale-100 font-gothic mt-5">
+                        <b className='max-w-3xl mx-auto text-xl font-gothic'>Are you a video editing wizard with a passion for storytelling?</b>
+                        <p className='my-5 max-w-3xl mx-auto text-xl font-gothic'>We're on the hunt for talented individuals to join our dynamic team. Hypecut is more than just a workplace; it's a creative hub where your skills can shine.</p>
+                        <button className="bg-primary text-white text-lg font-bold tracking-widest py-3 px-5 select-none hover:scale-105 active:scale-100 font-gothic mt-5">
                             See Open Positions
                         </button>
                     </div>
